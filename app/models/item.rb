35161ce573e5c05766422685671a83486d023915
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-27 13:07:52 +0000

    バリデーション、リレーション記述　

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  has_one_attached:image
+  has_one_attached :image
+  
+  belongs_to :genre
+  has_many :cart_items
+  has_many :order_details
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
