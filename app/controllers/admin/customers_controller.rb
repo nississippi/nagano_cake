dedec79fa5e1fd059fab4299068883dc082b97c8
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-28 04:54:21 +0000

    会員詳細、編集、一覧画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,34 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id), notice: '会員情報の更新に成功しました'
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana,
+    :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :cart_items
   has_many :orders
   has_many :addresses
+
+  validates :last_name, :last_name_kana, :first_name, :first_name_kana,
+    :postal_code, :address, :telephone_number, :is_deleted, presence: true
 end
