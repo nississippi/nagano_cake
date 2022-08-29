0a61a5dcf23f43c9ac6b4209e7adc8d4dc927dc0
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-29 11:25:36 +0000

    アソシエーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :genre
-  has_many :cart_items
-  has_many :order_details
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 
   validates :name, presence: true
   validates :introduction, presence: true
