000060b2886ad58c6d0163af72a17b4feaef24b5
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-30 14:51:47 +0000

    注文機能途中　

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,16 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if @order.save
+      redirect_to orders_confirm_path
+    else
+      render :new
+    end
   end
 
   def confirm
