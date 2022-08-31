8e7c90b515691d9554a659c6fd653f77c5256c5a
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-31 16:11:02 +0000

    注文画面作成中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,18 +2,29 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
+    cart_items = current_customer.cart_items.all
     if @order.save
+      cart_items.each do |cart|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart.item_id
+        order_detail.order_id = @order.id
+        order_detail.tax_included_price = cart.item.with_tax_price
+        order_detail.amount = cart.amount
+        order_detail.save
+      end
       redirect_to orders_confirm_path
+      cart_items.destroy_all
     else
       render :new
     end
   end
 
   def confirm
+    @order = Order.new(order_params)
   end
 
   def complete
@@ -23,5 +34,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping_name, :shipping_address, :shipping_postal_code,
+    :billing_amount, :payment,)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,11 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment: { credit_card: 0, transfer: 1 }
-  enum order_status: { waitng_for_payment: 0, payment_confirmed: 1, in_production: 2, preparing_to_ship: 3, shipped: 4 }
+  enum order_status:
+    { waitng_for_payment: 0, payment_confirmed: 1,
+    in_production: 2, preparing_to_ship: 3, shipped: 4 }
+
+  validates :shipping_name, :shipping_address, :shipping_postal_code, :billing_amount, :payment,
+  presence: true
+
 end
