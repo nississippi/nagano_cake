0461b282b08778747acd07ae5fcb2b6900117526
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-09-11 05:24:05 +0000

    製作ステータス実装中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,14 +3,22 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
     @total = 0
+    @total_amount = 0
   end
 
   def update
     order = Order.find(params[:id])
-    status = params[:order][:order_status]
+    order_detail = OrderDetail.find(params[:id])
+    if params[:order][:order_status].exists?
+      status = params[:order][:order_status]
+      order.update(order_status: status)
+    elsif params[:order_detail][:crafting_status].exists?
+      status = params[:order_detail][:crafting_status]
+      order_detail.update(crafting_status: status)
+    end
 
-    order.update(order_status: status)
-    binding.pry
     redirect_to admin_order_path(order)
+
+
   end
 end
