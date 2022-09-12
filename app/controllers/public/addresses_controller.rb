9fb195116abc506d80b20b5934b75506d0b43ba4
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-09-12 14:04:45 +0000

    配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -31,6 +31,7 @@ class Public::AddressesController < ApplicationController
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
+    redirect_to addresses_path
   end
 
   private
