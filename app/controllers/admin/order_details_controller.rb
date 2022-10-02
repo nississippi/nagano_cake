951c7ff62853a69c2e3dc228828a2204888af95e
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-10-02 08:18:04 +0000

    ステータス連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def update
     order_detail = OrderDetail.find(params[:id])
     order = Order.find(order_detail.order_id)
     status = params[:order_detail][:crafting_status]
     order_detail.update(crafting_status: status)
+    case order_detail.crafting_status
+      when "in_production"
+				order_detail.order.update(order_status: "in_production")
+			when "complete"
+			if order_detail.order.order_details.all?{|order_detail| order_detail.crafting_status == "complete"}
+				order_detail.order.update(order_status: "preparing_to_ship")
+			end
+		end
     redirect_to admin_order_path(order)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
@@ -12,6 +12,9 @@ class Admin::OrdersController < ApplicationController
     order = Order.find(params[:id])
     status = params[:order][:order_status]
     order.update(order_status: status)
+    if order.order_status == "payment_confirmed"
+      order.order_details.update_all(crafting_status: 1)
+    end
     redirect_to admin_order_path(order)
   end
 end
